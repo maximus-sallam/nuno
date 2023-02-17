5b5c960dfd7cb8732bf045391f9821c346eeafe7
Maximus Sallam <maximus.sallam@gmail.com>
2023-02-16 23:36:40 -0600

    added player and tile classes.

---
+++ player.py
@@ -0,0 +1,8 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('images/').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
