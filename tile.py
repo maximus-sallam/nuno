cef23c9f7470164e4f5e1183c46a10558724247e
Maximus Sallam <maximus.sallam@gmail.com>
2023-02-19 15:56:15 -0600

    Fixed object offset for objects larger than 64 x 64.

--- tile.py
+++ tile.py
@@ -7,5 +7,9 @@ class Tile(pygame.sprite.Sprite):
 		super().__init__(groups)
 		self.sprite_type = sprite_type
 		self.image = surface
-		self.rect = self.image.get_rect(topleft=pos)
+		if sprite_type == 'objects':
+			# perform an offset
+			self.rect = self.image.get_rect(topleft=(pos[0], pos[1] - TILE_SIZE))
+		else:
+			self.rect = self.image.get_rect(topleft=pos)
 		self.hitbox = self.rect.inflate(0, -10)
