1a8efc203dada01485534afe1ce15403187200a3
Maximus Sallam <maximus.sallam@gmail.com>
2023-02-19 13:16:11 -0600

    added and loaded background image (map)

--- level.py
+++ level.py
@@ -45,13 +45,21 @@ class YSortCameraGroup(pygame.sprite.Group):
 		self.half_height = self.display_surface.get_size()[1] // 2
 		self.offset = pygame.math.Vector2()
 
+		# creating the floor
+		self.floor_surf = pygame.image.load('assets/images/map/ground.png').convert()
+		self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
+
 	def custom_draw(self, player):
 
 		# getting the offset
 		self.offset.x = player.rect.centerx - self.half_width
 		self.offset.y = player.rect.centery - self.half_height
 
+		# drawing the floor
+		floor_offset_pos = self.floor_rect.topleft - self.offset
+		self.display_surface.blit(self.floor_surf, floor_offset_pos)
+
 		# for sprite in self.sprites():
 		for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-			offset_position = sprite.rect.topleft - self.offset
-			self.display_surface.blit(sprite.image, offset_position)
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image, offset_pos)
