7650c55bb0f94f155a0ef099217b087146bc6126
Maximus Sallam <maximus.sallam@gmail.com>
2023-02-19 23:02:11 -0600

    Added weapon images, weapon settings, and weapon class.

--- settings.py
+++ settings.py
@@ -3,3 +3,12 @@ WIDTH = 1280
 HEIGHT = 720
 FPS = 60
 TILE_SIZE = 64
+
+# weapons
+weapon_data = {
+    'sword': {'cooldown': 100, 'damage': 15, 'image': 'assets/images/weapons/sword/full.png'},
+    'lance': {'cooldown': 400, 'damage': 30, 'image': 'assets/images/weapons/lance/full.png'},
+    'axe': {'cooldown': 300, 'damage': 20, 'image': 'assets/images/weapons/axe/full.png'},
+    'rapier': {'cooldown': 50, 'damage': 8, 'image': 'assets/images/weapons/rapier/full.png'},
+    'sai': {'cooldown': 80, 'damage': 10, 'image': 'assets/images/weapons/sai/full.png'}
+}
