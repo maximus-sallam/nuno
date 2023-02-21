371bf0f5c3c01a82f46a33a1cd075c7cd621105e
Maximus Sallam <maximus.sallam@gmail.com>
2023-02-20 23:30:47 -0600

    added UI font, style, and colors

--- settings.py
+++ settings.py
@@ -4,6 +4,25 @@ HEIGHT = 720
 FPS = 60
 TILE_SIZE = 64
 
+# user interface
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = 'assets/font/joystix.ttf'
+UI_FONT_SIZE = 18
+
+# general colors
+WATER_COLOR = '#71DDEE'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
 # weapons
 weapon_data = {
     'sword': {'cooldown': 100, 'damage': 15, 'image': 'assets/images/weapons/sword/full.png'},
--- ui.py
+++ ui.py
@@ -4,7 +4,10 @@ from settings import *
 
 class UI:
     def __init__(self):
-        pass
+
+        # general
+        self.display_surface = pygame.display.get_surface()
+        self.font
 
     def display(self, player):
         pass
