b43e1c33f23a58126608134abfb5813876b5a247
Maximus Sallam <maximus.sallam@gmail.com>
2023-02-28 10:59:02 -0600

    removed import of debug from level.py

--- level.py
+++ level.py
@@ -2,7 +2,6 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-from debug import debug
 from support import *
 from random import choice
 from weapon import Weapon
